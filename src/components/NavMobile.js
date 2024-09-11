713ebed2c3af767031e6bfb7054930b252045fa5
Arsen <arsen.zuna01@gmail.com>
2024-09-11 23:59:06 +0200

    Second commit

--- src/components/NavMobile.js
+++ src/components/NavMobile.js
@@ -1,7 +1,28 @@
 import React from 'react';
+//import nav data
+import {navData} from "../data";
+//import components
+import Socials from "./Socials";
 
 const NavMobile = () => {
-  return <div>NavMobile</div>;
+	//destructure data
+	const {items} = navData;
+	return (
+		<nav className='w-full h-full flex flex-col justify-evenly overflow-hidden'>
+			<ul className='flex flex-col justify-center items-center gap-y-6 py-6 mb-8'>
+				{items.map((item, index) => {
+					return (
+						<li key={index}>
+							<a className='text-2xl font-primary link hover:border-b-2 hover:border-dark transition duration-400' href={item.href}>{item.name}</a>
+						</li>
+					)
+				})}
+			</ul>
+			<div className='text-2xl'>
+				<Socials/>
+			</div>
+		</nav>
+	)
 };
 
 export default NavMobile;
--- src/data.js
+++ src/data.js
@@ -1,5 +1,5 @@
 // import images
-import Logo from '../src/img/header/logo.svg';
+import Logo from '../src/img/header/logo.png';
 import GalleryImg1 from '../src/img/gallery/1.png';
 import GalleryImg2 from '../src/img/gallery/2.png';
 import GalleryImg3 from '../src/img/gallery/3.png';
@@ -38,14 +38,14 @@ export const navData = {
 
 export const socialData = [
   { href: '/', icon: <GrFacebookOption /> },
-  { href: '/', icon: <IoLogoInstagram /> },
+  { href: 'https://www.instagram.com/garage.art.studio/', icon: <IoLogoInstagram /> },
   { href: '/', icon: <IoLogoPinterest /> },
   { href: '/', icon: <IoLogoTwitter /> },
   { href: '/', icon: <IoLogoYoutube /> },
 ];
 
 export const heroData = {
-  title: 'I’m Void Sir.',
+  title: 'Garage Art Studio',
   subtitle:
     'Tattoos have their own unique power and magic. They not only beautify the body but also the psyche.',
   btnText: 'read more',
@@ -53,17 +53,15 @@ export const heroData = {
 };
 
 export const aboutData = {
-  title: 'My Story:',
+  title: 'Our Story:',
   subtitle1:
     'In velit arcu posuere integer. Dolor sit amet, consectetur nun adipiscing elit. Duis porttitor massa tellus hac vel ante sit sed scelerisque praesent duis volutpat laoreet.',
   subtitle2:
     'Nisl, sit molestie commodo congue. Etiam lectus risus in amet. Commodo molestie fames etiam aenean sed. Pellentesque et venenatis amet, tellus hac vel adipiscing sit. Placerat vitae nisl viverra faucibus tincidunt habitasse amet. Nunc, velit nunc, scelerisque imperdiet nunc.',
-  btnText: 'Know more',
-  btnIcon: <IoMdArrowForward />,
 };
 
 export const galleryData = {
-  title: 'Check my gallery:',
+  title: 'Check our gallery:',
   btnText: 'View all',
   btnIcon: <IoMdArrowForward />,
   images: [
@@ -143,41 +141,41 @@ export const testimonialData = [
 ];
 
 export const contactData = {
-  title: 'Get in touch with me:',
+  title: 'Get in touch with us:',
   info: [
     {
-      title: 'LA office',
+      title: 'Tiranë, Albania',
       subtitle:
         'In velit arcu posuere integer sit amet, consectetur nun adipiscing elit. Duis porttitor massa tellus',
       address: {
         icon: <FaMapMarkerAlt />,
-        name: '784 Norman Street, Los Angeles',
+        name: 'Rruga Sami Frashëri, përballë Drejtorisë së Policisë.',
       },
       phone: {
         icon: <FaPhoneAlt />,
-        number: '+49 93 30493943',
+        number: '+355 69 95 50 077',
       },
       email: {
         icon: <FaEnvelope />,
-        address: 'contact@yourcompany.com',
+        address: 'garageartstudio@gmail.com',
       },
       link: 'Get location',
     },
     {
-      title: 'NYC office',
+      title: 'Larvik, Norway',
       subtitle:
         'Sit amet, consectetur nun in velit arcu posuere integer. Adipiscing elit duis porttitor massa tellus',
       address: {
         icon: <FaMapMarkerAlt />,
-        name: '1630 Elm Drive, New York City',
+        name: 'Brannvaktsgate 21, 3256 Larvik, Norway',
       },
       phone: {
         icon: <FaPhoneAlt />,
-        number: '+49 34 36573355',
+        number: '+47 40 19 28 54',
       },
       email: {
         icon: <FaEnvelope />,
-        address: 'contact@yourcompany.com',
+        address: 'booking.letsink@gmail.com',
       },
       link: 'Get location',
     },
@@ -192,7 +190,7 @@ export const contactData = {
 
 export const footerData = {
   about: {
-    title: 'About void tattoo',
+    title: 'About Garage Art Studio',
     subtitle:
       'Sit amet, consectetur nun in velit arcu posuere integer. Adipiscing elit duis porttitor massa tellus',
     address: {
@@ -201,7 +199,7 @@ export const footerData = {
     },
     phone: {
       icon: <FaPhoneAlt />,
-      number: '+49 34 36573355',
+      number: '+355 69 95 50 077',
     },
     email: {
       icon: <FaEnvelope />,
@@ -211,10 +209,10 @@ export const footerData = {
   links: {
     title: 'Useful links',
     items: [
-      { href: '/', name: 'About me' },
-      { href: '/', name: 'My gallery' },
-      { href: '/', name: 'My services' },
-      { href: '/', name: 'Contact me' },
+      { href: '/', name: 'About Us' },
+      { href: '/', name: 'Our Gallery' },
+      { href: '/', name: 'Our Services' },
+      { href: '/', name: 'Contact Us' },
     ],
   },
   program: {
